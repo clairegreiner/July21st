62d8cab51fb7cc3adc9e6d577d2749d850fe8892
clairegreiner <clairegreiner@gmail.com>
2016-07-21 15:21:54 -0400

    Bang!

--- src/com/greiner/Fizzbuzz.java
+++ src/com/greiner/Fizzbuzz.java
@@ -1,50 +1,50 @@
 package com.greiner;
 
+// I get the concept, but need to police the curly braces
 public class Fizzbuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		for (int i = 110; i> 0; i--) {
+		// Where the hell does the arg end? // i mod 3 and Fizz
+
+		for (int i = 110; i > 0; i--) {
+
+			if (((i % 3) == 0) && ((i % 5) == 0) && ((i % 7) == 0)) {
+				System.out.println("FizzBuzzBang");
 			
-			 if (((i % 3)==0) && ((i %5) == 0) && ((i %7 )==0)) {
-					System.out.println("FizzBuzzBang"); 
-				// i mod 3	
-			if ((i % 3)==0) {
-				System.out.println("Fizz"); 
+			} else if (((i % 3) == 0) && ((i % 7) == 0)) {
+				System.out.println("FizzBang");
+
+				// i mod 5 and Buzz
+
+			} else if (((i % 5) == 0) && ((i % 7) == 0)) {
+				System.out.println("BuzzBang");
+
+				// i mod 5 and Buzz
+			} else if (((i % 3) == 0) && ((i % 5) == 0)) {
+				
+			} else if ((i % 5) == 0) {
+				System.out.println("Buzz");
+			
+			} else if ((i % 7) == 0) {
+				System.out.println("Bang");
+			
+			} else if ((i % 3) == 0) {
+				System.out.println("Fizz");
 			}
 				
-			} else if (((i % 3)==0) && ((i %7 )==0)) {
-					System.out.println("FizzBuzzBang");
-				
-					//i mod 5
-			}   if ((i % 5)==0) {
-				System.out.println("Buzz"); 
-				
-				if ((i % 7)==0) {
-					System.out.println("Bang");
-				}		
-			} else if (((i % 3)==0) && ((i%5) == 0)) {
-				
+
+				// i mod 7 and Bang
+
+				// } else if (((i%5) == 0) && ((i%7 )==0)) {
+				// System.out.println("FizzBuzzBang");
+				// If all the else stmts fails, then just print the n
+
+			 else {
+				System.out.println(i);
 			}
-				
-			}
-				// i mod 5
-			} else if (((i%5) == 0) && ((i%7 )==0)) {
-					System.out.println("FizzBuzzBang");
-			// If all the else stmts fails, then just print the n
-						
-			} else {
-					System.out.println(i);
-				}
-		}}
+		}
+	}
 
-//check in to the 1st repo this morning
-
-
-
-
-
+	// check in to the 1st repo this morning
 
 }
